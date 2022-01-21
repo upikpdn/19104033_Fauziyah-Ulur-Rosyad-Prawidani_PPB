a17ff8a36c373160fbbcc167f3a014ae797cd21a
FAUZIYAH ULUR ROSYAD PRAWIDANI <fprawidani@gmail.com>
2022-01-21 17:35:40 +0700

    Modul 10

---
+++ Modul 10/app/src/main/java/com/fauziyah_19104033/modul10/data/Quote.kt
@@ -0,0 +1,12 @@
+package com.rifqi_19104031.modul10.data
+
+import android.os.Parcelable
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
